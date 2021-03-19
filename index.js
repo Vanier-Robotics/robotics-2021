2955b8a7f0949651fabf467df16dbc3d87473360
shadowlerone <andrae.lewis@gmail.com>
2021-03-18 21:06:49 -0400

    Created rendering JS file

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 const pug = require('pug');
 var fs = require('fs');
 
-let rawdata = fs.readFileSync("src/data/archives/archives.json");
-let data = JSON.parse(rawdata);
-
-let articles = [data.video];
-let langs = ["en", "fr"];
-langs.forEach(lang => {
-	var dir = `build/${lang}/archives`;
+generate = function(dir, fp, src, data){
 	if (!fs.existsSync(dir)) {
 		fs.mkdirSync(dir, { recursive: true });
 	}
-	fs.writeFile(`${dir}/index.html`,
-			pug.renderFile("src/pug/pages/archives.pug",
-				{articles:articles, lang:lang}),
+	fs.writeFile(`${dir}/${fp}`,
+			pug.renderFile(src,
+				data),
 			function (err) {
 				if (err) throw err;
 				console.log('Saved!');
 			}
 		)
+}
+let langs = ["en", "fr"];
+
+let raw_arachives = fs.readFileSync("src/data/archives/archives.json");
+let raw_members = fs.readFileSync("src/data/roster/members-test.json");
+let archives = JSON.parse(raw_arachives);
+let roster = JSON.parse(raw_members);
+
+let articles = [archives.video, archives.video, archives.video];
+
+generate(`build`, `index.html`,`src/pug/pages/landingpage.pug`, {})
+
+
+langs.forEach(lang => {
+	generate (
+		`build/${lang}/archives`, 
+		"index.html", 
+		"src/pug/pages/archives.pug", 
+		{
+			articles:articles, 
+			lang:lang
+		})
+})
+roster.teams.forEach(t => {
+	langs.forEach(lang => {
+		generate(
+			`build/${lang}/roster-test-2/${t}`, 
+			"index.html", 
+			"src/pug/pages/roster/team.pug", 
+			{
+				fmembers: roster.members.filter(m => m.teams.includes(t)),
+				lang:lang
+			})
+	})
 })
