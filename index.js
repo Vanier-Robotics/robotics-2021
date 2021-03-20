27edb400d27833034eddd04e9eaa799bd8a54a1f
shadowlerone <andrae.lewis@gmail.com>
2021-03-20 18:55:58 -0400

    Partially rewrote the site generator

--- index.js
+++ index.js
@@ -1,50 +1,92 @@
 const pug = require('pug');
 var fs = require('fs');
+require('log-timestamp');
+const chokidar = require('chokidar');
 
-generate = function(dir, fp, src, data){
+const archives_fp = "src/data/archives/archives.json";
+const members_fp = "src/data/roster/members-test.json";
+const translation_fp = "src/data/text.json";
+
+function get (fp) {
+	var rawdata = fs.readFileSync(fp);
+	return JSON.parse(rawdata);
+}
+function get_archives() {
+	return get(archives_fp);
+}
+function get_members() {
+	return get(members_fp);
+}
+function get_translation() {
+	return get(translation_fp);
+}
+
+generate = function (dir, fp, src, data) {
 	if (!fs.existsSync(dir)) {
 		fs.mkdirSync(dir, { recursive: true });
 	}
+	data["text"] = get_translation();
+	// console.log(data)
 	fs.writeFile(`${dir}/${fp}`,
-			pug.renderFile(src,
-				data),
-			function (err) {
-				if (err) throw err;
-				console.log('Saved!');
-			}
-		)
+		pug.renderFile(src,
+			data),
+		function (err) {
+			if (err) throw err;
+			// console.log('Saved!');
+		}
+	)
 }
-let langs = ["en", "fr"];
 
-let raw_arachives = fs.readFileSync("src/data/archives/archives.json");
-let raw_members = fs.readFileSync("src/data/roster/members-test.json");
-let archives = JSON.parse(raw_arachives);
-let roster = JSON.parse(raw_members);
-
-let articles = [archives.video, archives.video, archives.video];
-
-generate(`build`, `index.html`,`src/pug/pages/landingpage.pug`, {})
-
-
-langs.forEach(lang => {
-	generate (
-		`build/${lang}/archives`, 
-		"index.html", 
-		"src/pug/pages/archives.pug", 
-		{
-			articles:articles, 
-			lang:lang
-		})
-})
-roster.teams.forEach(t => {
+function generate_archives(langs, articles) {
 	langs.forEach(lang => {
 		generate(
-			`build/${lang}/roster/${t}`, 
-			"index.html", 
-			"src/pug/pages/roster/team.pug", 
+			`build/${lang}/archives`,
+			"index.html",
+			"src/pug/pages/archives.pug",
 			{
-				fmembers: roster.members.filter(m => m.teams.includes(t)),
-				lang:lang
+				articles: articles,
+				lang: lang
 			})
 	})
-})
+}
+function generate_roster(langs, roster) {
+	roster.teams.forEach(t => {
+		langs.forEach(lang => {
+			generate(
+				`build/${lang}/roster/${t}`,
+				"index.html",
+				"src/pug/pages/roster/team.pug",
+				{
+					fmembers: roster.members.filter(m => m.teams.includes(t)),
+					lang: lang,
+					t:t
+				})
+		})
+	})
+}
+
+let langs = ["en", "fr"];
+
+if (process.argv.includes("-w") == true || process.argv.includes("--watch") == true) {
+	chokidar.watch("src/**/*").on('change', (event, path) => {	
+		var archives = get_archives()
+		var roster = get_members();
+		let articles = [
+			archives.video, archives.kiosk
+		];
+		generate(`build`, `index.html`, `src/pug/pages/landingpage.pug`, {})
+		generate_archives(langs, articles)
+		generate_roster(langs, roster)
+		console.log("Generated Website!")
+	})
+
+} else {
+	var archives = get_archives()
+	var roster = get_members();
+	let articles = [
+		archives.video, archives.kiosk
+	];
+	generate(`build`, `index.html`, `src/pug/pages/landingpage.pug`, {})
+	generate_archives(lang, articles)
+	generate_roster(lang, roster)
+}
